0366da0174b8bc1cc237ff0182877e4c5c6a4f2d
Siyuan Chen <46427716+chen-siyuan@users.noreply.github.com>
2024-05-04 21:14:33 -0400

    Stop using long long and relabel sentinels

--- src/sequential/dinics.cc
+++ src/sequential/dinics.cc
@@ -1,12 +1,13 @@
 #include "sequential/dinics.h"
 
 namespace {
+    constexpr auto NONE{-1};
     constexpr auto INF{std::numeric_limits<int>::max()};
 
     void build_layers(const Network& network, std::vector<int>& dist) {
         const auto& [num_verts, source, sink, num_edges, edges, adj]{network};
 
-        std::fill(std::begin(dist), std::end(dist), INF);
+        std::fill(std::begin(dist), std::end(dist), NONE);
         std::queue<int> queue{};
 
         dist[source] = 0;
@@ -19,16 +20,16 @@ namespace {
             const auto child_dist{dist[u] + 1};
             for (const auto i: adj[u])
                 if (const auto& [from, to, cap, flow]{edges[i]};
-                    flow < cap and dist[to] == INF) {
+                    flow < cap and dist[to] == NONE) {
                     dist[to] = child_dist;
                     queue.push(to);
                 }
         }
     }
 
-    long long push_flow(
+    int push_flow(
         Network& network, std::vector<int>& dist, std::vector<int>& curr,
-        const int u, const long long flow_in
+        const int u, const int flow_in
     ) {
         auto& [num_verts, source, sink, num_edges, edges, adj]{network};
 
@@ -38,30 +39,27 @@ namespace {
         const auto degree{static_cast<int>(std::size(adj[u]))};
         const auto child_dist{dist[u] + 1};
 
-        long long flow_out{};
+        int flow_out{};
         for (auto& i{curr[u]}; i < degree; ++i) {
             const auto j{adj[u][i]};
             if (auto& [from, to, cap, flow]{edges[j]};
                 flow < cap and dist[to] == child_dist) {
-                const auto flow_pushed{static_cast<int>(push_flow(
+                const auto flow_pushed{push_flow(
                     network, dist, curr,
-                    to, std::min(
-                        flow_in - flow_out,
-                        static_cast<long long>(cap - flow)
-                    )
-                ))};
+                    to, std::min(flow_in - flow_out, cap - flow)
+                )};
 
                 flow += flow_pushed;
                 edges[j ^ 1].flow -= flow_pushed;
 
                 if ((flow_out += flow_pushed) == flow_in) {
                     i += static_cast<int>(flow == cap);
-                    return static_cast<int>(flow_out);
+                    return flow_out;
                 }
             }
         }
 
-        return static_cast<int>(flow_out);
+        return flow_out;
     }
 }
 
@@ -73,7 +71,7 @@ namespace sequential {
         std::vector<int> curr(num_verts);
 
         while (true) {
-            if (build_layers(network, dist); dist[sink] == INF)
+            if (build_layers(network, dist); dist[sink] == NONE)
                 return;
             std::fill(std::begin(curr), std::end(curr), 0);
             while (push_flow(network, dist, curr, source, INF) > 0ll);
